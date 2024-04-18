2d1cf4a7d2e8ad494ae9cef964f3208aa356d982
Alina Mihaela <ciopecalina@gmail.com>
2024-04-18 21:30:05 +0300

    Added some Sets exs

--- java-algorithmic-exercises/src/sets/DistributeCandies.java
+++ java-algorithmic-exercises/src/sets/DistributeCandies.java
@@ -1,4 +1,30 @@
 package sets;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DistributeCandies {
+    public static int distributeCandies(int[] candyType) {
+        int maxNoOfDiffTypes = 0;
+        int maxNoOfCandies = candyType.length / 2;
+        Set<Integer> set = new HashSet<>();
+        for (int type : candyType) {
+            set.add(type);
+        }
+        maxNoOfDiffTypes = set.size();
+
+        return Math.min(maxNoOfCandies, maxNoOfDiffTypes);
+    }
+
+    public static void main(String[] args) {
+
+        int[] candyType1 = {1, 1, 2, 2, 3, 3};
+        System.out.println("Output for candyType = [1, 1, 2, 2, 3, 3]: " + distributeCandies(candyType1)); // Output: 3
+
+        int[] candyType2 = {1, 1, 2, 3};
+        System.out.println("Output for candyType = [1, 1, 2, 3]: " + distributeCandies(candyType2)); // Output: 2
+
+        int[] candyType3 = {6, 6, 6, 6};
+        System.out.println("Output for candyType = [6, 6, 6, 6]: " + distributeCandies(candyType3)); // Output: 1
+    }
 }
