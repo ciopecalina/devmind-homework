4843537206b5de6b28d8d52119130d565b47c9fb
Alina Mihaela <ciopecalina@gmail.com>
2024-05-14 21:11:28 +0300

    Added solution

--- java-algorithmic-exercises/src/general/HouseRobber.java
+++ java-algorithmic-exercises/src/general/HouseRobber.java
@@ -2,7 +2,19 @@ package general;
 
 public class HouseRobber {
 
-    public static void main(String[] args) {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
 
+        int rob = 0;
+        int norob = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int newRob = norob + nums[i];
+            int newNoRob = Math.max(norob, rob);
+            rob = newRob;
+            norob = newNoRob;
+        }
+        return Math.max(rob, norob);
     }
 }
---
+++ java-algorithmic-exercises/src/general/SelfDividingNumbers.java
@@ -0,0 +1,27 @@
+package general;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfDividingNumbers {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> list = new ArrayList<>();
+        int n1 = left, n2 = right;
+        for (int i = left; i <= right; i++) {
+            if (check(i))
+                list.add(i);
+        }
+        return list;
+    }
+
+    public boolean check(int n) {
+        int digit = n;
+        while (n > 0) {
+            int div = n % 10;
+            if (div == 0) return false;
+            if (digit % div != 0) return false;
+            n = n / 10;
+        }
+        return true;
+    }
+}
